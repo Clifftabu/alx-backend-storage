c84e60934d8e69e6edcc137ce464a53959ab2883
CLIFFTABU <clifftabu@gmail.com>
2025-07-13 01:14:27 +0300

    task 5 correction

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,20 +2,19 @@
 
 import redis
 import requests
-from typing import Callable
 
 r = redis.Redis()
 
 
 def get_page(url: str) -> str:
     """
-    Fetches the HTML content of a URL, caches it for 10 seconds,
-    and tracks the number of times the URL was accessed.
+    Fetch the HTML content of a URL, cache it for 10 seconds,
+    and track the number of times the URL was accessed in Redis.
     """
     r.incr(f"count:{url}")
-    cached = r.get(url)
-    if cached:
-        return cached.decode('utf-8')
+    cached_page = r.get(url)
+    if cached_page:
+        return cached_page.decode('utf-8')
     response = requests.get(url)
     r.setex(url, 10, response.text)
     return response.text
